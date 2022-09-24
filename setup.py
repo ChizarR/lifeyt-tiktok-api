6fabd350994654add450b8113d201ebf31024d64
ChizarR <chizarr27@gmail.com>
2022-09-24 23:19:35 +0200

    .

--- setup.py
+++ setup.py
@@ -2,11 +2,10 @@ from setuptools import find_packages, setup
 
 
 setup(
-    name="TikTokAPI",
-    packages=find_packages(include=["./tiktok_api/"]),
+    name="tiktok_api",
+    packages=find_packages(include=["tiktok_api"]),
     version="0.1.0",
     description="Unofficial TikTokAPI",
     author="ChizarR",
     license="MIT",
-    install_requires=[],
 )
